38c9385ca5315750dca95a179fdbeead986b8193
Ayush Newatia <ayush.newatia@icloud.com>
2016-08-11 17:04:37 +0100

    added extension to load test images

---
+++ AvatarImageViewTests/UIImage+TestUtils.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+TestUtils.swift
+//  AvatarImageView
+//
+//  Created by Ayush Newatia on 11/08/2016.
+//  Copyright © 2016 Ayush Newatia. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    convenience init?(namedInTest name: String) {
+        self.init(named: name,
+                  inBundle: NSBundle(forClass: self.dynamicType),
+                  compatibleWithTraitCollection: nil)
+    }
+}
