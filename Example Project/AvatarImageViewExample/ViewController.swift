916f6f38b85a7862ecc47f68213a62173c72f9c1
Ayush Newatia <ayush.newatia@icloud.com>
2016-08-12 14:50:19 +0100

    added example project

---
+++ Example Project/AvatarImageViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AvatarImageViewExample
+//
+//  Created by Ayush Newatia on 12/08/2016.
+//  Copyright © 2016 Spectrum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
